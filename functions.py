169d17de67d2196fd2d955d608098eaf76258529
pjrigali <peterjrigali@gmail.com>
2025-12-16 15:59:02 -0800

    Add read_docx function

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import csv
-
+import zipfile
+from xml.etree.cElementTree import XML
 
 # Read csv's.
 def read_csv(path: str, nrows: int = 0) -> list:
@@ -20,3 +21,23 @@ def read_csv(path: str, nrows: int = 0) -> list:
             return [next(csv_reader) for _ in range(nrows)]
         else:
             return [row for row in csv_reader]
+
+
+# Read in a word doc file.
+def read_docx(file_path: str) -> str:
+    """Takes a file location/name and returns the contents of a word document as str."""
+    # Capture the file.
+    wn = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
+    p, t = wn + 'p', wn + 't'
+    document = zipfile.ZipFile(file_path)
+    xml_content = document.read('word/document.xml')
+    document.close()
+    tree = XML(xml_content)
+
+    # Read in the paragraphs.
+    paragraphs = []
+    for paragraph in tree.iter(p):
+        texts = [node.text for node in paragraph.iter(t) if node.text]
+        if texts:
+            paragraphs.append(''.join(texts))
+    return '\n'.join(paragraphs)

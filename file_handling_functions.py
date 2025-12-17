e40ef405eae1b8a5026e808d1f1c284070111405
pjrigali <peterjrigali@gmail.com>
2025-12-16 17:27:00 -0800

    File renamed.

---
+++ file_handling_functions.py
@@ -0,0 +1,105 @@
+import csv
+import zipfile
+from xml.etree.cElementTree import XML
+from pathlib import Path
+
+
+# Read csv's.
+def read_csv(path: str, nrows: int = 0) -> list:
+    """Simple csv reader, fixes keys, returns the csv as a list of dicts."""
+    # Check if file is a csv.
+    if not path.endswith('.csv'):
+        path = path + '.csv'
+
+    with open(path, mode='r', encoding='utf-8-sig') as file:
+        csv_reader = csv.DictReader(file)
+
+        # Catch and fix key names.
+        if csv_reader.fieldnames:
+            csv_reader.fieldnames = [name.lstrip("\ufeff") for name in csv_reader.fieldnames]
+
+        # Only read in x lines.
+        if nrows:
+            return [next(csv_reader) for _ in range(nrows)]
+        else:
+            return [row for row in csv_reader]
+
+
+# Update file version. Useful for creating successive csv outputs.
+def update_file_version(folder_path: str, file_name: str) -> str:
+    """
+    Takes an input folder and file name, returns the next file version. This allow different versioning of files.
+    Nest this in the save_csv(file_path=update_file_version(folder_path='your folder', file_name='your filenanme'), data).
+    """
+    root = Path(folder_path) if folder_path else Path.cwd()
+    n = 1
+    while True:
+        n_filename = root / f'{file_name}_v{n}.csv'
+        if not n_filename.exists():
+            return str(n_filename)
+        n += 1
+
+# Old attempt for version updating.
+# import os
+# import re
+
+# # New function to handle versioning and saving the data
+# def update_file_version(folder_path: str, base_filename: str) -> str:
+#     # Get list of all files in the folder
+#     if folder_path:
+#         files = os.listdir(folder_path)
+#     else:
+#         files = os.listdir()
+   
+#     # Filter out the CSV files
+#     csv_files = [file for file in files if file.endswith('.csv') and file.startswith(base_filename)]
+#     pattern = re.compile(rf"^{re.escape(base_filename)}_v(\d+)\.csv$")
+   
+#     version_numbers = []
+#     for file in csv_files:
+#         match = pattern.match(file)
+#         if match:
+#             version_numbers.append(int(match.group(1)))
+   
+#     # Determine the next version number
+#     next_version = max(version_numbers, default=0) + 1
+
+#     return os.path.join(folder_path, f"{base_filename}_v{next_version}.csv")
+
+
+# Save a list of dictionaries to a csv.
+def save_csv(file_path: str, data: list, sort_columns: bool = False) -> None:
+    """Takes a list of dictionaries and saves them to a csv. Assumes all the dict's have the same keys."""
+    with open(file_path, mode='w', encoding='utf-8-sig', newline='') as file:
+        # I know, first row? gross.
+        cols = data[0].keys()
+    
+        if sort_columns:
+            cols.sort()
+        
+        csv_writer = csv.DictWriter(file, fieldnames=cols, restval='', extrasaction='ignore')        
+        csv_writer.writeheader()
+        csv_writer.writerows(data)
+
+    print(f'File created: ({file_path})')
+    return None
+
+
+# Read in a word doc file.
+def read_docx(file_path: str) -> str:
+    """Takes a file location/name and returns the contents of a word document as str."""
+    # Capture the file.
+    wn = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
+    p, t = wn + 'p', wn + 't'
+    document = zipfile.ZipFile(file_path)
+    xml_content = document.read('word/document.xml')
+    document.close()
+    tree = XML(xml_content)
+
+    # Read in the paragraphs.
+    paragraphs = []
+    for paragraph in tree.iter(p):
+        texts = [node.text for node in paragraph.iter(t) if node.text]
+        if texts:
+            paragraphs.append(''.join(texts))
+    return '\n'.join(paragraphs)
